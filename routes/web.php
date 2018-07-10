c2446c9443f26b3fa25cbbfdedeb9cdce58f57a6
Sean-Paul McKee <sp@sean-paul.com>
2018-07-09 19:09:10 -0700

    Updated route for classes

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/students/manage', 'StudentsController@index')->name('students');
+Route::get('/classes/manage'), 'ClassesController@index')->name('classes');
